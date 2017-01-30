83d2d8e12722cb4bf2ca0283afabc739c956bc44
Joseph Duchesne <josephgeek@gmail.com>
2017-01-30 14:29:20 -0500

    Added material messages and material color visualization

--- scripts/visualize_segment_list.py
+++ scripts/visualize_segment_list.py
@@ -5,24 +5,48 @@ import roslib
 roslib.load_manifest("light_scan_sim")
 
 import rospy
-from light_scan_sim.msg import SegmentList
+from light_scan_sim.msg import SegmentList, MaterialList
 from visualization_msgs.msg import MarkerArray, Marker
 from geometry_msgs.msg import Vector3
 
 markers = MarkerArray()
+segments = None
+materials = None
 
-# Upate the global pose
+# Store materials and attempt to generate the marker list
+def material_callback(data):
+    global materials
+    materials = data.materials
+    print "Got material list with %d segments" % len(data.materials)
+    generate_markers()
+
+# Store segments and attempt to generate the marker list
 def segment_callback(data):
-    global markers
+    global segments
+    segments = data
+    print "Got segment list with %d segments" % len(data.segments)
+    generate_markers()
+
+# Combine materials and segments into a visualization marker array
+def generate_markers():
+    global markers, materials, segments
+
+    if materials is None:
+        print "Waiting for materials"
+        return
+
+    if segments is None:
+        print "Waiting on segments"
+        return
 
     markers.markers = []
 
-    for i in range(len(data.segments)):
-      segment = data.segments[i]
+    for i in range(len(segments.segments)):
+      segment = segments.segments[i]
 
       # Generate marker for each segment
       m = Marker();
-      m.header.frame_id = data.frame_id
+      m.header.frame_id = segments.frame_id
       m.header.stamp = rospy.get_rostime()
       m.id = i
       m.type = 4  # Line list
@@ -36,10 +60,14 @@ def segment_callback(data):
       m.action = 0
       m.points.append(Vector3(segment.start[0], segment.start[1], 0))
       m.points.append(Vector3(segment.end[0], segment.end[1], 0))
+      if (segment.type < len(materials)):
+        m.color.r = materials[segment.type].color[0]
+        m.color.g = materials[segment.type].color[1]
+        m.color.b = materials[segment.type].color[2]
       m.color.a = 1.0
       markers.markers.append(m)
 
-    print "Got segment list with %d segments" % len(markers.markers)
+    print "Generated markers" 
 
 # Publish the 
 if __name__ == '__main__':
@@ -48,6 +76,7 @@ if __name__ == '__main__':
     # Todo: Load the material list
 
     rospy.Subscriber(rospy.get_param('~input_topic', '/segments'), SegmentList, segment_callback)
+    rospy.Subscriber(rospy.get_param('~materials_topic', '/materials'), MaterialList, material_callback)
     pub = rospy.Publisher(rospy.get_param('~output_topic', '/segments_vis'), MarkerArray, queue_size=1)
 
     rate = rospy.Rate(1.0)
